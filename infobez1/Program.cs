a8c8b8c30a5faca76de59c96295d945e1b7fe21d
huchupuri <squaer120@gmail.com>
2024-12-04 16:11:30 +0300

    Добавьте файлы проекта.

---
+++ infobez1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+
+namespace cezar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1; 
+            int key;
+
+            Console.WriteLine("Введите предложение,которое нужно зашифровать:");
+            string sentence = Console.ReadLine();
+
+            Console.WriteLine("Введите ключ:");
+            while (!int.TryParse(Console.ReadLine(), out key))
+            {
+                Console.WriteLine("Введите целое число");
+            }
+            key %= 30;
+
+            StringBuilder answer = new StringBuilder();
+            string alfphabet = "АБВГДЕЖЗИКЛМНОПРСТУФХЦЧШЩЬЪЭЮЯ";
+            int m = alfphabet.Length;
+
+            int len = sentence.Length;
+            for (int i = 0; i < len; i++)
+            {
+                if (alfphabet.Contains(char.ToUpper(sentence[i])))
+                {
+
+                    if (key + alfphabet.IndexOf(char.ToUpper(sentence[i])) < m)
+                    {
+                        if (char.IsLower(sentence[i])) 
+                        { 
+                            answer.Append(char.ToLower(alfphabet[alfphabet.IndexOf(char.ToUpper(sentence[i])) + key]));
+                        }
+                        else
+                        {
+                            answer.Append(alfphabet[alfphabet.IndexOf(char.ToUpper(sentence[i])) + key]);
+                        }
+                    }
+                    else
+                    {
+                        if (char.IsLower(sentence[i]))
+                        {
+                            answer.Append(char.ToLower(alfphabet[Math.Abs(m - alfphabet.IndexOf(char.ToUpper(sentence[i])) - key)]));
+                        }
+                        else
+                        {
+                            answer.Append(alfphabet[Math.Abs(m - alfphabet.IndexOf(char.ToUpper(sentence[i])) - key)]);
+                        }
+                    }
+                }
+                else
+                {
+                    answer.Append(sentence[i]);
+                }
+            }
+            Console.WriteLine(answer);
+        }
+    }
+}
